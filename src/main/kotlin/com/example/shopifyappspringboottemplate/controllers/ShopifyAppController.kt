44070aad16894becdc5a5da7553d344093bc68df
t-kurihara <takuya.kurihara00@gmail.com>
2022-02-23 18:43:00 +0900

    Fix ModelAttribute Exception.

--- src/main/kotlin/com/example/shopifyappspringboottemplate/controllers/ShopifyAppController.kt
+++ src/main/kotlin/com/example/shopifyappspringboottemplate/controllers/ShopifyAppController.kt
@@ -1,15 +1,17 @@
 package com.example.shopifyappspringboottemplate.controllers
 
-import io.netty.handler.codec.http.HttpResponseStatus
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Mono
-import reactor.netty.http.server.HttpServerResponse
+import java.net.URI
 
 @RestController
 @RequestMapping("/shopifyApp")
@@ -17,39 +19,51 @@ class ShopifyAppController {
 
     @GetMapping("/install")
     fun install(
-        @RequestParam("shop") shop: String?,
-        response: HttpServerResponse,
-    ): Mono<ServerResponse> {
-
-        println("shop: $shop")
-
-        return if (shop == null) {
-            ServerResponse.status(HttpStatus.BAD_REQUEST).build()
-        } else {
-            val redirectUrl =
-                UriComponentsBuilder.fromHttpUrl("https://$shop/admin/oauth/authorize")
-                    .queryParam("client_id", System.getenv("SHOPIFY_APP_API_KEY"))
-                    .queryParam("scope", "read_products,write_products")
-                    .queryParam("redirect_uri", "https://${System.getenv("SHOPIFY_APP_DOMAIN")}/shopifyApp/callback")
-                    .build()
-                    .toUriString()
-
-            ServerResponse
-                .status(HttpStatus.TEMPORARY_REDIRECT)
-                .header("Location", redirectUrl)
+        @RequestParam(value = "shop") shop: String?,
+    ): Mono<ResponseEntity<Void>> = if (shop == null) {
+        Mono.just(ResponseEntity.badRequest().build())
+    } else {
+        val redirectUrl =
+            UriComponentsBuilder.fromHttpUrl("https://$shop/admin/oauth/authorize")
+                .queryParam("client_id", System.getenv("SHOPIFY_APP_API_KEY"))
+                .queryParam("scope", "read_products,write_products")
+                .queryParam("redirect_uri", "${System.getenv("SHOPIFY_APP_DOMAIN")}/shopifyApp/callback")
                 .build()
-        }
+                .toUri()
+
+        Mono.just(
+            ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
+                .location(redirectUrl)
+                .build()
+        )
     }
 
     @GetMapping("/callback")
     fun callback(
         @RequestParam("shop") shop: String?,
         @RequestParam("code") code: String?,
-        response: HttpServerResponse,
-    ): Mono<Void> = if (shop == null || code == null) {
-        response.sendNotFound()
+    ): Mono<ResponseEntity<Void>> = if (shop == null || code == null) {
+        Mono.just(ResponseEntity.badRequest().build())
     } else {
-
-        response.sendRedirect("https://$shop/admin/apps")
+        WebClient
+            .create()
+            .post()
+            .uri("https://$shop/admin/oauth/access_token")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(
+                mapOf(
+                    "code" to code,
+                    "client_id" to System.getenv("SHOPIFY_APP_API_KEY"),
+                    "client_secret" to System.getenv("SHOPIFY_APP_API_SECRET"),
+                )
+            )
+            .retrieve()
+            .bodyToMono<HashMap<String, String>>()
+            .log()
+            .thenReturn(
+                ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
+                    .location(URI("https://$shop/admin/apps"))
+                    .build()
+            )
     }
 }
