2b104ceaa05f04208e6b1d31c33e805b62155def
Takuya Kurihara <takuya.kurihara00@gmail.com>
2022-02-24 16:26:15 +0900

    Trim print functions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/example/shopifyappspringboottemplate/config/HMACVerificationFilter.kt
+++ src/main/kotlin/com/example/shopifyappspringboottemplate/config/HMACVerificationFilter.kt
@@ -1,13 +1,13 @@
 package com.example.shopifyappspringboottemplate.config
 
-import javax.crypto.Mac
-import javax.crypto.spec.SecretKeySpec
-import kotlin.experimental.and
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import kotlin.experimental.and
 
 class HMACVerificationFilter : WebFilter {
 
@@ -17,12 +17,10 @@ class HMACVerificationFilter : WebFilter {
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         val path = exchange.request.path.value()
         if (!path.startsWith("/shopifyApp", false)) {
-            println("path($path) is not required to be verified.")
             return chain.filter(exchange)
         }
 
         val hmac = exchange.request.queryParams.toSingleValueMap()[HMAC]
-        println("hmac($hmac)")
 
         val withoutHmac = exchange
             .request
@@ -32,8 +30,6 @@ class HMACVerificationFilter : WebFilter {
             .toSortedMap()
             .map { "${it.key}=${it.value}" }
             .joinToString("&")
-
-        println("withoutHmac($withoutHmac)")
 
         val mac = Mac.getInstance(algorithm)
         val keySpec = SecretKeySpec(
@@ -49,10 +45,8 @@ class HMACVerificationFilter : WebFilter {
         }
 
         return if (builder.toString() == hmac) {
-            println("hmac($hmac) is verified.")
             chain.filter(exchange)
         } else {
-            println("hmac($hmac), ${builder.toString()} is not verified.")
             exchange.response.statusCode = HttpStatus.UNAUTHORIZED
             return exchange.response.setComplete()
         }
